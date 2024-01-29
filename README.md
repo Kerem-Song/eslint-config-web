aa206d3281995b733c8eb2b87994ee561c2f378d
keremcalki@gmail.com <keremcalki@gmail.com>
2024-01-29 19:16:39 +0900

    feat: eslint tsconfig

---
+++ README.md
@@ -0,0 +1,7 @@
+# eslint-config-web
+
+웹 개발팀에서 사용하는 리액트 전용 eslint 및 tsconfig
+
+eslint - index.js
+
+tsconfig - tsconfig.json, cjs.json, esm.json (상황에 맞게 골라서 사용.)
---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:tailwindcss/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "react/prop-types": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.js"],
+      parser: "@typescript-eslint/parser",
+    },
+    {
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: ["*.js", "**.config.ts"],
+    },
+  ],
+};
