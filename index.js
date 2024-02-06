332e5f2ec8893110622b51ca6c4433bbf8f98af8
keremcalki@gmail.com <keremcalki@gmail.com>
2024-02-06 12:25:03 +0900

    fix: simple import sort groups

--- index.js
+++ index.js
@@ -36,7 +36,20 @@ module.exports = {
     "simple-import-sort/imports": [
       "error",
       {
-        groups: [["^react"], ["^antd"], ["^@?\\w"], ["@/(.*)"], ["^[./]"]],
+        groups: [
+          // Packages `react` related packages come first.
+          ["^react", "^@?\\w"],
+          // Internal packages.
+          ["^(@|components)(/.*|$)"],
+          // Side effect imports.
+          ["^\\u0000"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.?(css)$"],
+        ],
       },
     ],
     "simple-import-sort/exports": "error",
